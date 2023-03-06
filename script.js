f30620a95a4a1d3dec098ff2571ff7b4e1a29134
Spremic <dspremic1@gmail.com>
2023-03-06 17:58:28 +0100

    commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let input = document.querySelector('.text')
+let span = document.querySelector('span')
+let btn = document.querySelector('.result')
+btn.addEventListener('click', function () {
+let value = input.value
+let value2 = value.replace(/\s/g, '');
+let length = value2.length
+
+
+span.innerHTML = `${length}`
+let test = document.querySelector
+})
+
+
+
+
+
+    
